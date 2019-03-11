538dbce65753d814b16bce23f5f52f14a0d15049
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-03-11 16:47:36 -0700

    No GpuMats

--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
-	cv::cuda::GpuMat gpuMat = cv::cuda::GpuMat();
 	cv::Mat mat = cv::Mat();
-	gpuMat.upload(mat);
 
 	// TODO Some Processing
 
-	gpuMat.download(mat);
 	std::cout << mat << std::endl;
 	return 0;
 }
