3d89b0fd4d5ccfaca1e9c97be2eac799803c4362
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-03-11 12:24:12 -0700

    GpuMat Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	cv::cuda::GpuMat gpuMat = cv::cuda::GpuMat();
+	cv::Mat mat = cv::Mat();
+	gpuMat.upload(mat);
+
+	// TODO Some Processing
+
+	gpuMat.download(mat);
+	std::cout << mat << std::endl;
+	return 0;
 }
