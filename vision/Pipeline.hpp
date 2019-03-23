3900803a6892c2dc0a4a9406c670351607394faa
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-03-22 17:49:10 -0700

    Cleanup and Pipeline Fixes

--- vision/Pipeline.hpp
+++ vision/Pipeline.hpp
@@ -1,16 +1,17 @@
-//
-// Created by Owen Leather on 3/14/2019.
-//
-
 #ifndef VISION_PIPELINE_H
 #define VISION_PIPELINE_H
 
-
 #include "PipelineData.hpp"
+#include "Target.hpp"
 
 class Pipeline {
 public:
-    PipelineData pipeline(cv::Mat img);
+	PipelineData pipeline(cv::Mat img);
+
+	void s(const char *name, cv::Mat mat);
+
+private:
+	double cachedAngle;
 };
 
 
