d815077d10117dd97ef5afd697c75e9a2cc99c60
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-03-22 17:48:53 -0700

    Cleanup and Pipeline Fixes

--- vision/Filters.cpp
+++ vision/Filters.cpp
@@ -1,43 +1,39 @@
-//
-// Created by Owen Leather on 3/14/2019.
-//
-
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 #include "Filters.hpp"
 
-cv::Scalar GREEN_MIN_HSV = cv::Scalar(50, 105, 0);
-cv::Scalar GREEN_MAX_HSV = cv::Scalar(180, 255, 255);
+cv::Scalar GREEN_MIN_HSV = cv::Scalar(47, 0, 147);
+cv::Scalar GREEN_MAX_HSV = cv::Scalar(102, 255, 255);
 
-cv::Mat Filters::filterColor(cv::Mat img) {
-    cv::cvtColor(img, img, cv::COLOR_BGR2HSV);
-    cv::inRange(img,GREEN_MIN_HSV,GREEN_MAX_HSV, img);
-    return img;
+void Filters::filterColor(cv::Mat img) {
+	cv::cvtColor(img, img, cv::COLOR_BGR2HSV);
+	cv::inRange(img, GREEN_MIN_HSV, GREEN_MAX_HSV, img);
 }
 
-std::vector<std::vector<cv::Point>> Filters::filterSize(std::vector<std::vector<cv::Point>> ctr, int minHeight, int maxHeight) {
-    std::vector<std::vector<cv::Point>> newCtr;
-    for (int i = 0; i < ctr.size(); i++) {
-        cv::Rect boundrectNew = cv::boundingRect(cv::Mat(ctr[i]));
-        if (boundrectNew.height > minHeight &&  boundrectNew.height < maxHeight) {
-            newCtr.push_back(ctr[i]);
-        }
-    }
-    ctr.clear();
-    return newCtr;
+std::vector<std::vector<cv::Point>>
+Filters::filterSize(std::vector<std::vector<cv::Point>> ctr, int minHeight, int maxHeight) {
+	std::vector<std::vector<cv::Point>> newCtr;
+	for (int i = 0; i < ctr.size(); i++) {
+		cv::Rect boundrectNew = cv::boundingRect(cv::Mat(ctr[i]));
+		if (boundrectNew.height > minHeight && boundrectNew.height < maxHeight) {
+			newCtr.push_back(ctr[i]);
+		}
+	}
+	ctr.clear();
+	return newCtr;
 }
 
 std::vector<std::vector<cv::Point>> Filters::filterVerts(std::vector<std::vector<cv::Point>> ctr, int vertCount) {
-    std::vector<std::vector<cv::Point>> newCtr;
+	std::vector<std::vector<cv::Point>> newCtr;
 
-    for (int i = 0; i < ctr.size(); i++) {
+	for (int i = 0; i < ctr.size(); i++) {
 
-        if(ctr[i].size() == vertCount){
+		if (ctr[i].size() == vertCount) {
 
-            newCtr.push_back(ctr[i]);
-        }
+			newCtr.push_back(ctr[i]);
+		}
 
-    }
-    ctr.clear();
-    return newCtr;
+	}
+	ctr.clear();
+	return newCtr;
 }
