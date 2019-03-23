21e9cc9435b44a28559e328e9aa4a0116e6761da
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-03-22 17:49:00 -0700

    Cleanup and Pipeline Fixes

--- vision/Math.cpp
+++ vision/Math.cpp
@@ -6,53 +6,52 @@
 #include "Math.hpp"
 
 std::string Math::getDistanceAndAngle(Target target, double imgWidth) {
-    //Get left and right diagonal heights
-    cv::RotatedRect leftRect = cv::minAreaRect(target.leftTarget);
-    cv::RotatedRect rightRect = cv::minAreaRect(target.rightTarget);
-    double leftHeight = leftRect.size.height;
-    double rightHeight = rightRect.size.height;
-    //Get left and right distance
-    double leftDistance = getDiagonalDistance(leftHeight);
-    double rightDistnce = getDiagonalDistance(rightHeight);
-    //Get average distance to target
-    double targetDistance = (leftDistance + rightDistnce) / 2;
-    //Get yaw to target
-    double leftYaw = getYaw(leftRect.boundingRect().x, imgWidth);
-    double rightYaw = getYaw(leftRect.boundingRect().x, imgWidth);
-    double targetYaw = (leftYaw + rightYaw) /2;
-    //Get relative target offset
-    double leftOffset = getRelativeOffset(leftDistance, leftYaw);
-    double rightOffset = getRelativeOffset(rightDistnce, rightYaw);
-    double offset = getRelativeOffset(targetDistance, targetYaw);
-    //Get direct distance to target
-    double directDistance = sqrt((offset*offset) + (targetDistance * targetDistance));
-    //Get target legs
-    double oLeg = leftDistance-rightDistnce;
-    double aLeg = leftOffset-rightOffset;
-    //Get drive to point
-    double driveToX = offset + oLeg;
-    double driveToY = targetDistance - aLeg;
-    //Get new dist and angle
-    double newDist = sqrt((driveToX*driveToX) + (driveToY * driveToY));
-    double newAngle = atan(driveToX / driveToY);
+	//Get left and right diagonal heights
+	cv::RotatedRect leftRect = cv::minAreaRect(target.leftTarget);
+	cv::RotatedRect rightRect = cv::minAreaRect(target.rightTarget);
+	double leftHeight = leftRect.size.height;
+	double rightHeight = rightRect.size.height;
+	//Get left and right distance
+	double leftDistance = getDiagonalDistance(leftHeight);
+	double rightDistnce = getDiagonalDistance(rightHeight);
+	//Get average distance to target
+	double targetDistance = (leftDistance + rightDistnce) / 2;
+	//Get yaw to target
+	double leftYaw = getYaw(leftRect.boundingRect().x, imgWidth);
+	double rightYaw = getYaw(leftRect.boundingRect().x, imgWidth);
+	double targetYaw = (leftYaw + rightYaw) / 2;
+	//Get relative target offset
+	double leftOffset = getRelativeOffset(leftDistance, leftYaw);
+	double rightOffset = getRelativeOffset(rightDistnce, rightYaw);
+	double offset = getRelativeOffset(targetDistance, targetYaw);
+	//Get direct distance to target
+	double directDistance = sqrt((offset * offset) + (targetDistance * targetDistance));
+	//Get target legs
+	double oLeg = leftDistance - rightDistnce;
+	double aLeg = leftOffset - rightOffset;
+	//Get drive to point
+	double driveToX = offset + oLeg;
+	double driveToY = targetDistance - aLeg;
+	//Get new dist and angle
+	double newDist = sqrt((driveToX * driveToX) + (driveToY * driveToY));
+	double newAngle = atan(driveToX / driveToY);
 
 
-
-    std::string distAndYaw = "d" + std::to_string(newDist) +  "a" + std::to_string(newAngle);
-    return distAndYaw;
+	std::string distAndYaw = "d" + std::to_string(newDist) + "a" + std::to_string(newAngle);
+	return distAndYaw;
 
 }
 
 double Math::getRelativeOffset(double targetDistance, double yaw) {
-    return (targetDistance * tan(yaw));
+	return (targetDistance * tan(yaw));
 }
 
 double Math::getDiagonalDistance(double targetPixelHeight) {
-    return ((TARGET_HEIGHT * CALC_FOCAL_LENGTH) / targetPixelHeight);
+	return ((TARGET_HEIGHT * CALC_FOCAL_LENGTH) / targetPixelHeight);
 }
 
 double Math::getYaw(int xPos, double imgWidth) {
-    double xOffset = (imgWidth / 2) - xPos;
-    double yaw = atan2(xOffset, CALC_FOCAL_LENGTH);
-    return (yaw);
+	double xOffset = (imgWidth / 2) - xPos;
+	double yaw = atan2(xOffset, CALC_FOCAL_LENGTH);
+	return (yaw);
 }
