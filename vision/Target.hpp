ed974b5ae8cfbb2fae7e222ccd14ef745b82b918
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-03-22 17:49:17 -0700

    Cleanup and Pipeline Fixes

--- vision/Target.hpp
+++ vision/Target.hpp
@@ -1,16 +1,14 @@
-//
-// Created by Owen Leather on 3/14/2019.
-//
-
 #ifndef VISION_TARGET_H
 #define VISION_TARGET_H
 
 #include <opencv2/core/types.hpp>
 
-class Target{
+struct Target {
+	std::vector<cv::Point> leftTarget;
+	std::vector<cv::Point> rightTarget;
+
 public:
-    std::vector<cv::Point> leftTarget;
-    std::vector<cv::Point> rightTarget;
+	int getAngle() const;
 };
 
 #endif //VISION_TARGET_H
