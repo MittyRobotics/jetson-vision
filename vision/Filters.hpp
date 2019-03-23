59b050570b39270cb34acafdc51a7cd5094434eb
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-03-22 17:48:57 -0700

    Cleanup and Pipeline Fixes

--- vision/Filters.hpp
+++ vision/Filters.hpp
@@ -1,18 +1,17 @@
-//
-// Created by Owen Leather on 3/14/2019.
-//
-
 #ifndef VISION_FILTERS_H
 #define VISION_FILTERS_H
 
 
 class Filters {
 public:
-    cv::Mat filterColor(cv::Mat img);
+	void filterColor(cv::Mat img);
+
 public:
-    std::vector<std::vector<cv::Point>> filterSize(std::vector<std::vector<cv::Point>> ctr, int minHeight, int maxHeight);
+	std::vector<std::vector<cv::Point>>
+	filterSize(std::vector<std::vector<cv::Point>> ctr, int minHeight, int maxHeight);
+
 public:
-    std::vector<std::vector<cv::Point>> filterVerts(std::vector<std::vector<cv::Point>> ctr, int vertCount);
+	std::vector<std::vector<cv::Point>> filterVerts(std::vector<std::vector<cv::Point>> ctr, int vertCount);
 };
 
 
